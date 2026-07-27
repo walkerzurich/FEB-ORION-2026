ed3963f584984f7b4a0893c0d552d8d3dd3f9004
Muhammad Fiqri <muhammadfiqri@MacBook-Pro-Muhammad.local>
2026-07-27 11:01:09 +0700

    test 2

--- src/pages/OpeningPage.jsx
+++ src/pages/OpeningPage.jsx
@@ -65,7 +65,7 @@ export default function OpeningPage() {
                 letterSpacing: "0.02em",
               }}
             >
-              Journey Of Economicssss
+              Journey Of Economics
             </h1>
           </motion.div>
 
